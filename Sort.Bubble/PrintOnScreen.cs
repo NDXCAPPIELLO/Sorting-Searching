2ef230f63da3d035c7940e53ac912e364cf94667
NDXCAPPIELLO <andres_alcap@hotmail.com>
2019-01-27 17:53:17 -0300

    UpdateNombres

--- Sort.Bubble/PrintOnScreen.cs
+++ Sort.Bubble/PrintOnScreen.cs
@@ -6,13 +6,29 @@ namespace MainModule
 {
     public class PrintOnScreen
     {
-        public static void Imprimir(int[] lista)
+        public static void ImprimirVector(int[] lista)
         {
             for (int i = 0; i < lista.Length; i++)
             {
                 Console.WriteLine(lista[i]);
             }
             Console.WriteLine(" ");
+        }
+
+        public static void ImprimirPosicion(int? pos)
+        {           
+            if (pos != null)
+            {
+                Console.WriteLine("El numero 5 está en la posición: " + pos);
+            }
+            else {
+                Console.Write("No se ha encontrado el elemento");
+            }            
+        }
+
+        public static void ImprimirCantidad(int cant)
+        {
+            Console.WriteLine("El numero 5 aparece " + cant + " veces.");
         }
     }
 }
--- Sort.Bubble/Program.cs
+++ Sort.Bubble/Program.cs
@@ -10,11 +10,11 @@ namespace MainModule
         {                        
             int[] lista = { 9, 2, 1, 7, 4, 8, 0, 3, 5, 6 };
 
-            PrintOnScreen.Imprimir(Burbujeo.OrdernarPorBurbujeo(Auxiliar.pasarCopiaDeVector(lista))); //Ordenamiento - Burbujeo             
-            PrintOnScreen.Imprimir(Seleccion.OrdenarPorSeleccionDelMinimo(Auxiliar.pasarCopiaDeVector(lista))); //Ordenamiento - Selección (del Minimo)
+            PrintOnScreen.ImprimirVector(Burbujeo.OrdernarPorBurbujeo(Auxiliar.pasarCopiaDeVector(lista))); //Ordenamiento - Burbujeo             
+            PrintOnScreen.ImprimirVector(Seleccion.OrdenarPorSeleccionDelMinimo(Auxiliar.pasarCopiaDeVector(lista))); //Ordenamiento - Selección (del Minimo)
 
-            Console.WriteLine("El numero 5 aparece " + Lineal.ContarLinealmente(lista, 5) + " veces."); // Búsqueda - Conteo
-            Console.WriteLine("El numero 5 está en la posición: " + Lineal.BuscarLinealmenteElPrimero(lista, 5)); //Búsqueda - Primero                       
+            PrintOnScreen.ImprimirCantidad(Lineal.ContarLinealmente(lista, 50)); // Búsqueda - Conteo
+            PrintOnScreen.ImprimirPosicion(Lineal.BuscarLinealmenteElPrimero(lista, 50)); //Búsqueda - Primero                       
 
             Console.ReadKey();
         }
