82f713a8f6e7767b1ee368107c24daa3d0d1f6c0
NDXCAPPIELLO <andres_alcap@hotmail.com>
2019-01-28 11:20:32 -0300

    Update nombre

--- Sort.Bubble/Program.cs
+++ Sort.Bubble/Program.cs
@@ -10,7 +10,7 @@ namespace MainModule
         {
             int[] lista = { 9, 2, 1, 7, 4, 8, 0, 3, 5, 6 };
 
-            PrintOnScreen.ImprimirVector(Burbujeo.OrdenarPorBurbujeo(lista)); //Ordenamiento - Burbujeo             
+            PrintOnScreen.ImprimirVector(Burbujeo.OrdenarPorBurbujeo(Auxiliar.pasarCopiaDeVector(lista))); //Ordenamiento - Burbujeo             
             PrintOnScreen.ImprimirVector(Seleccion.OrdenarPorSeleccionDelMinimo(Auxiliar.pasarCopiaDeVector(lista))); //Ordenamiento - Selección (del Minimo)
 
             PrintOnScreen.ImprimirCantidad(Lineal.ContarLinealmente(lista, 50)); // Búsqueda - Conteo
